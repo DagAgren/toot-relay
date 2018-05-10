cfc8eabc8bacaf5beb8b66bff16d22307ff3d65c
Dag Ågren <paracelsus@gmail.com>
2018-05-10 23:11:36 +0300

    Implement a few more headers and proper responses

--- toot-relay.go
+++ toot-relay.go
@@ -8,6 +8,8 @@ import (
 	"net/http"
 	"os"
 	"strings"
+	"strconv"
+	"time"
 
 	"github.com/sideshow/apns2"
 	"github.com/sideshow/apns2/certificate"
@@ -20,7 +22,8 @@ var cert tls.Certificate
 func handler(writer http.ResponseWriter, request *http.Request) {
 	components := strings.Split(request.URL.Path, "/")
 
-	token := components[2]
+	notification := &apns2.Notification{}
+	notification.DeviceToken = components[2]
 
 	buffer := new(bytes.Buffer)
     buffer.ReadFrom(request.Body)
@@ -29,7 +32,10 @@ func handler(writer http.ResponseWriter, request *http.Request) {
 	encodedBytes := make([]byte, z85.EncodedLen(len(body)))
 	_, err := z85.Encode(encodedBytes, body)
 	if err != nil {
-		log.Panic("Encode error:", err)
+		writer.WriteHeader(500)
+		fmt.Fprintln(writer, "Encode error:", err)
+		log.Println("Encode error:", err)
+		return
 	}
 	encodedString := string(encodedBytes)
 
@@ -39,17 +45,46 @@ func handler(writer http.ResponseWriter, request *http.Request) {
 		payload.Custom("x", strings.Join(components[3:], "/"))
 	}
 
-	notification := &apns2.Notification{}
-	notification.DeviceToken = token
 	notification.Payload = payload
+
+	if seconds := request.Header.Get("TTL"); seconds != "" {
+		if ttl, err := strconv.Atoi(seconds); err == nil {
+			notification.Expiration = time.Now().Add(time.Duration(ttl) * time.Second)
+		}
+	}
+
+	if topic := request.Header.Get("Topic"); topic != "" {
+		notification.CollapseID = topic
+	}
+
+	switch request.Header.Get("Urgency") {
+		case "very-low", "low":
+			notification.Priority = apns2.PriorityLow
+		default:
+			notification.Priority = apns2.PriorityHigh
+	}
 
 	client := apns2.NewClient(cert).Development()
 	res, err := client.Push(notification)
 	if err != nil {
-		log.Panic("Push error:", err)
+		writer.WriteHeader(500)
+		fmt.Fprintln(writer, "Push error:", err)
+		log.Println("Push error:", err)
+		return
 	}
 
-	fmt.Fprintf(writer, "Sent notification to %s -> %v %v %v", token, res.StatusCode, res.ApnsID, res.Reason)
+	if res.Sent() {
+		writer.Header().Add("Location", fmt.Sprintf("https://not-supported/%v", res.ApnsID))
+		writer.WriteHeader(201)
+		log.Printf("Sent notification to %s -> %v %v %v", notification.DeviceToken, res.StatusCode, res.ApnsID, res.Reason)
+		log.Println("Expiration:", notification.Expiration)
+		log.Println("Priority:", notification.Priority)
+		log.Println("CollapseID:", notification.CollapseID)
+	} else {
+		writer.WriteHeader(res.StatusCode)
+		fmt.Fprintln(writer, res.Reason)
+		log.Printf("Failed to send: %v %v %v\n", res.StatusCode, res.ApnsID, res.Reason)
+	}
 }
 
 func main() {
