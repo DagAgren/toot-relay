71112173859b7ee68cbb641613c59991ffdff9ce
Dag Ågren <paracelsus@gmail.com>
2018-05-12 23:12:35 +0300

    Add support for further encryption headers

--- toot-relay.go
+++ toot-relay.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"encoding/base64"
 	"bytes"
 	"crypto/tls"
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
@@ -18,6 +20,22 @@ import (
 )
 
 var cert tls.Certificate
+
+func main() {
+	var err error
+	cert, err = certificate.FromP12File("toot-relay.p12", "")
+	if err != nil {
+    	log.Fatal("Cert error:", err)
+	}
+
+	http.HandleFunc("/relay-to/", handler)
+
+	if _, err := os.Stat("toot-relay.crt"); !os.IsNotExist(err) {
+		log.Fatal(http.ListenAndServeTLS(":42069", "toot-relay.crt", "toot-relay.key", nil))
+	} else {
+		log.Fatal(http.ListenAndServe(":42069", nil))
+	}
+}
 
 func handler(writer http.ResponseWriter, request *http.Request) {
 	components := strings.Split(request.URL.Path, "/")
@@ -49,6 +67,33 @@ func handler(writer http.ResponseWriter, request *http.Request) {
 	}
 
 	notification.Payload = payload
+
+	switch request.Header.Get("Content-Encoding") {
+		case "aesgcm":
+			if publicKey, err := encodedValue(request.Header, "Crypto-Key", "dh"); err == nil {
+				payload.Custom("k", publicKey)
+			} else {
+				writer.WriteHeader(500)
+				fmt.Fprintln(writer, "Error retrieving public key:", err)
+				log.Println("Error retrieving public key:", err)
+				return
+			}
+
+			if salt, err := encodedValue(request.Header, "Encryption", "salt"); err == nil {
+				payload.Custom("s", salt)
+			} else {
+				writer.WriteHeader(500)
+				fmt.Fprintln(writer, "Error retrieving salt:", err)
+				log.Println("Error retrieving salt:", err)
+				return
+			}
+		//case "aes128gcm": // No further headers needed. However, not implemented on client side so return 415.
+		default:
+			writer.WriteHeader(415)
+			fmt.Fprintln(writer, "Unsupported Content-Encoding:", request.Header.Get("Content-Encoding"))
+			log.Println("Unsupported Content-Encoding:", request.Header.Get("Content-Encoding"))
+			return		
+	}
 
 	if seconds := request.Header.Get("TTL"); seconds != "" {
 		if ttl, err := strconv.Atoi(seconds); err == nil {
@@ -90,18 +135,39 @@ func handler(writer http.ResponseWriter, request *http.Request) {
 	}
 }
 
-func main() {
-	var err error
-	cert, err = certificate.FromP12File("toot-relay.p12", "")
+func encodedValue(header http.Header, name, key string) (string, error) {
+	keyValues := parseKeyValues(header.Get(name))
+	value, exists := keyValues[key]
+	if !exists {
+		return "", errors.New(fmt.Sprintf("Value %s not found in header %s", key, name))
+	}
+
+	bytes, err := base64.RawURLEncoding.DecodeString(value)
 	if err != nil {
-    	log.Fatal("Cert error:", err)
+		return "", err
 	}
 
-	http.HandleFunc("/relay-to/", handler)
-
-	if _, err := os.Stat("toot-relay.crt"); !os.IsNotExist(err) {
-		log.Fatal(http.ListenAndServeTLS(":42069", "toot-relay.crt", "toot-relay.key", nil))
-	} else {
-		log.Fatal(http.ListenAndServe(":42069", nil))
+	encodedBytes := make([]byte, z85.EncodedLen(len(bytes)))
+	if _, err := z85.Encode(encodedBytes, bytes); err != nil {
+		return "", err
 	}
+
+	return string(encodedBytes), nil
 }
+
+func parseKeyValues(values string) map[string]string {
+  f := func(c rune) bool {
+		return c == ';'
+	}
+
+  entries := strings.FieldsFunc(values, f)
+
+  m := make(map[string]string)
+  for _, entry := range entries {
+    parts := strings.Split(entry, "=")
+    m[parts[0]] = parts[1]
+  }
+
+  return m
+}
+
